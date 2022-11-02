86c51196ee5e138a4b2ea886b254d1225766a27e
Nedudigital <segunchinedu2000@gmail.com>
2022-11-02 07:01:04 +0100

    new template

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2020. Design by{' '}
+            <a href='http://www.templatewire.com' rel='nofollow'>
+              Veron Digital
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
